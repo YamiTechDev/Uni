802aad7842849395cd895e2358449bd028711353
Тетлер Стас <stasselez@yandex.ru>
2026-09-15 20:44:20 +0300

    First commit, create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
+// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Нажмите <shortcut actionId="ShowIntentionActions"/>, когда курсор находится на подсвеченном тексте,
+        // чтобы увидеть предложения IntelliJ IDEA по исправлению.
+        System.out.printf("Hello and welcome!");
+
+
+    }
+}
