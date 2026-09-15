4054a93686b6bac9326ea55b8d23e33199808d8a
Тетлер Стас <stasselez@yandex.ru>
2026-09-15 22:18:23 +0300

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        byte varByte = 110;
+        short varShort = 1_000;
+        int varInt = 123456;
+        long varLong = 2368476;
+        float varFloat = 11211.12f;
+        double varDouble = 11211.22d;
+        boolean varBoolean = false;
+        char varChar = 'A';
+    }
+}
